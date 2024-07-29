ede61d7cd311e2d89445ca3950b7406814ba94ff
Adam Messner <adam.messner@intouchsol.com>
2024-07-29 11:44:20 -0500

    fixing logic

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -66,7 +66,7 @@ function autolinkModals(element) {
 
   const documentLinks = element.querySelectorAll('a');
   for(let i = 0; i < documentLinks.length; i++){
-    let link = documentLinks.length[i]
+    let link = documentLinks[i];
     if (link && link.href && link.href.includes('/modals/') && link.href.includes('forced')) {
       const { openModal } = import(`${window.hlx.codeBasePath}/blocks/modal/modal.js`);
       openModal(link.href);
