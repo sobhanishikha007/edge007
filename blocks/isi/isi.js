6a9ec0440afd3f804e4f17a295f32beb6d1e49ed
Adam Messner <adam.messner@intouchsol.com>
2024-08-09 10:22:44 -0500

    Adding table block

--- blocks/isi/isi.js
+++ blocks/isi/isi.js
@@ -17,4 +17,15 @@ export default async function decorate(block) {
   while (fragment.firstElementChild) isi.append(fragment.firstElementChild);
 
   block.append(isi);
+
+  // load safety as fragment
+  const safetyPath = '/safety';
+  const safetyFragment = await loadFragment(safetyPath);
+
+  // decorate isi DOM
+  const safety = document.createElement('div');
+  safety.classList.add('safety-bar');
+  while (safetyFragment.firstElementChild) safety.append(safetyFragment.firstElementChild);
+
+  block.append(safety);
 }
